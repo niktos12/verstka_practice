a85001d26274535f836e2a71ddc9a4d8df7461e1
niktos12 <nikitosss121207@gmail.com>
2023-11-20 19:48:16 +0400

    .

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,17 @@ $(document).ready(function() {
     $('.card').flip({
         trigger: 'click',
     });
-    $(this).on('click', function() {
+    $('.card-btn').on('click', function() {
         $('.card').flip();
         // $('.card').flip(true);
       });
 
-      $(this).on('click', function() {
-        $('.card').flip();
-        // $('.back').css('display', 'flex');
-        // $('.card').flip(false);
-      });
+      // $(this).on('click', function() {
+      //   $('.card').flip();
+      //   // $('.back').css('display', 'flex');
+      //   // $('.card').flip(false);
+      // });
+    // $('#card').flip({
+    //   trigger: 'click',
+    // });
 });

8b5458400249ec332d30da459c585c805822ffa4
niktos12 <nikitosss121207@gmail.com>
2023-11-24 09:25:11 +0400

    /

--- scripts/app.js
+++ scripts/app.js
@@ -4,15 +4,11 @@ $(document).ready(function() {
     });
     $('.card-btn').on('click', function() {
         $('.card').flip();
-        // $('.card').flip(true);
       });
-
-      // $(this).on('click', function() {
-      //   $('.card').flip();
-      //   // $('.back').css('display', 'flex');
-      //   // $('.card').flip(false);
-      // });
-    // $('#card').flip({
-    //   trigger: 'click',
-    // });
+      
+});
+const btnShow = $("#all-cards-btn")
+const card = $("#card")
+btnShow.on("click",function(){
+        console.log($(window).width())
 });
